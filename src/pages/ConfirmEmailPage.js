2d3bcda2a12664826a0da23e8e3ad79363f53cbe
Kyt Edol <ellacruz.llibnitze@gmail.com>
2024-04-19 13:04:33 +0000

    ConfirmEmailPage.js edited online with Bitbucket

--- src/pages/ConfirmEmailPage.js
+++ src/pages/ConfirmEmailPage.js
@@ -1,1 +1,5 @@
 //add code here
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Box,Grid, Typography,TextField, Button } from '@mui/material'
+import { Link } from 'react-router-dom'
